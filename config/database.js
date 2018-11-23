0569a7d287fe00468da8a2ffa791503d75eff78a
dayoung9650 <dayoung9650@gmail.com>
2018-11-24 04:45:07 +0900

    수정# port number

---
+++ config/database.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  host        : 'root.cpgtkojuz64e.ap-northeast-2.rds.amazonaws.com',
+  port        : 3306,
+  user        : 'root',
+  password    : 'HEaaFCG7mrsUc6c',
+  database    : 'pleaseSunshine',
+  serverId    : 256,
+  minInterval : 200,
+  acquireTimeout: 300,
+  connectionLimit: 100,
+  connectTimeout: 2000,
+  queueLimit: 0,
+  debug: true,
+  waitForConnection: true,
+  dateStrings: true
+}; 
+
+
