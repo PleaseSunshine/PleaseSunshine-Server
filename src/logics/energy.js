9a1428a511713f6b09fad4f15f749dc39faed8ed
dayoung9650 <dayoung9650@gmail.com>
2018-11-24 02:38:48 +0900

    pull req 테스트

--- src/logics/energy.js
+++ src/logics/energy.js
@@ -9,7 +9,6 @@ module.exports = {
     //lat, lon 로 일사량 평균 구함 
     //e1, e2, e3 과 angle로 태양광 구함
     getSunshine : async function (lat, lon, angle) {
-        
     },
     getEnergy : async function (energy, angle) {
         return EnergyCalculator(energy, angle)
